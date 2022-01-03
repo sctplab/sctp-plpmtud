2c820c57f4296c05893e7af230f457ffb9ae1d3b
Timo Voelker <timo.voelker@fh-muenster.de>
2022-01-03 17:18:42 +0100

    add probe_mtu in struct sctp_heartbeat_info_param, add struct sctp_pad_chunk

--- netinet/sctp_header.h
+++ netinet/sctp_header.h
@@ -104,8 +104,7 @@ struct sctp_heartbeat_info_param {
 	uint32_t random_value2;
 	uint8_t addr_family;
 	uint8_t addr_len;
-	/* make sure that this structure is 4 byte aligned */
-	uint8_t padding[2];
+	uint16_t probe_mtu;
 	char address[SCTP_ADDRMAX];
 } SCTP_PACKED;
 
@@ -324,6 +323,12 @@ struct sctp_shutdown_chunk {
 /* Shutdown Acknowledgment (SHUTDOWN ACK) */
 struct sctp_shutdown_ack_chunk {
 	struct sctp_chunkhdr ch;
+} SCTP_PACKED;
+
+/* Padding Chunk (PAD) */
+struct sctp_pad_chunk {
+	struct sctp_chunkhdr ch;
+	/* padding data follow */
 } SCTP_PACKED;
 
 /* Operation Error (ERROR) */
